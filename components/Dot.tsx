876bc561f8b64c170e44607a7f51588bede0a5b0
Marco Mojica <marco@mojica.de>
2024-07-02 13:32:07 +0200

    Add color and size props for better reuse of shapes

--- components/Dot.tsx
+++ components/Dot.tsx
@@ -2,30 +2,36 @@ import { useCallback, useState } from "react";
 
 import { Container, Graphics, Text } from "@pixi/react";
 
+import { ColorSource } from "pixi.js";
 import { TextStyle } from "@pixi/text";
 import { Graphics as PixiGraphics } from "@pixi/graphics";
-import { TEXT_GRADIENT } from "pixi.js";
 
 type DotProps = {
   position: [number, number];
+  size?: number;
   text?: string;
-  isStart?: boolean;
+  color?: ColorSource;
 };
 
-export default function Dot({ position, isStart = false, text }: DotProps) {
+export default function Dot({
+  position,
+  size = 10,
+  text,
+  color = 0xffffff,
+}: DotProps) {
   const [poninterOver, setPointerOver] = useState<boolean>(false);
 
   const draw = useCallback(
     (g: PixiGraphics) => {
       g.clear();
-      g.beginFill(isStart ? 0xff0000 : 0xffffff);
-      g.drawCircle(0, 0, poninterOver ? 20 : 15);
+      g.beginFill(color, poninterOver ? 1 : 0.99);
+      g.drawCircle(0, 0, poninterOver ? 20 : size);
     },
-    [isStart, poninterOver]
+    [color, poninterOver, size]
   );
 
   return (
-    <Container position={position}>
+    <Container position={position} zIndex={poninterOver ? 999 : 0}>
       <Graphics
         draw={draw}
         onpointerenter={() => setPointerOver(true)}
@@ -33,15 +39,18 @@ export default function Dot({ position, isStart = false, text }: DotProps) {
       />
       {poninterOver && (
         <Text
-          text={`${text || ""}
-x: ${Math.trunc(position[0])}
-y: ${Math.trunc(position[1])}`}
-          position={[20, 20]}
+          text={`${text || ""} (${Math.trunc(position[0])},${Math.trunc(
+            position[1]
+          )})`}
+          position={[25, -24]}
+          anchor={[0, 0]}
+          angle={0}
           style={
             new TextStyle({
               // align: "center",
               // fontFamily: '"Source Sans Pro", Helvetica, sans-serif',
-              fontSize: 50,
+              fontSize: 30,
+              // lineHeight: 20,
               // fontWeight: "400",
               fill: ["#ffffff"], // gradient
               // fillGradientStops: [0, 0.1, 1, 0],
@@ -54,8 +63,8 @@ y: ${Math.trunc(position[1])}`}
               // dropShadowBlur: 4,
               // dropShadowAngle: Math.PI / 6,
               // dropShadowDistance: 6,
-              // wordWrap: true,
-              // wordWrapWidth: 440,
+              wordWrap: true,
+              wordWrapWidth: 300,
             })
           }
         />
--- components/LineAndDots.tsx
+++ components/LineAndDots.tsx
@@ -1,7 +1,5 @@
-import { Container } from "@pixi/react";
 import Dot from "./Dot";
 import Line from "./Line";
-import { useEffect, useState } from "react";
 
 type LineProps = {
   start: [number, number];
@@ -11,25 +9,16 @@ type LineProps = {
 };
 
 export default function LineAndDots({
-  start: initialStart,
-  end: initialEnd,
+  start,
+  end,
   expectation,
   experience,
 }: LineProps) {
-  const [start, setStart] = useState(initialStart);
-  useEffect(() => {
-    // setTimeout(() => setStart(([x, y]) => [(x + 10) % 1920, y]), 1000 / 30);
-  }, [start]);
-
-  const [end, setEnd] = useState(initialEnd);
-  useEffect(() => {
-    // setTimeout(() => setEnd(([x, y]) => [x, (y + 10) % 1080]), 1000 / 30);
-  }, [end]);
   return (
-    <Container>
-      <Line start={start} end={end} />
-      <Dot text={expectation} position={start} isStart />
-      <Dot text={experience} position={end} />
-    </Container>
+    <>
+      <Line start={start} end={end} color={0x3333ff} />
+      <Dot text={expectation} position={start} color={0x33ff33} size={8} />
+      <Dot text={experience} position={end} color={0xff3333} size={8} />
+    </>
   );
 }
